ce19132ec71304fd534cc31c24fe53b72b6b5818
Mwaniki Njagi <46248187+MwanikiNjagi@users.noreply.github.com>
2022-09-22 18:46:28 +0300

    Learning structs

--- structs.c
+++ structs.c
@@ -9,20 +9,16 @@ struct Person{
 };
 
 int main(){
-    struct Person person1;
-
-    person1.salary = 60000.02;
-    person1.age = 24;
-    person1.first_name = "Mwaniki";
-    person1.second_name = "Njagi";
-    
+    struct Person person1 = {.salary = 60000.02, .age = 24, .first_name = "Mwaniki", .second_name = "Njagi"};
+    struct Person person2 = {.salary = 5000.03, .age = 56, .first_name = "John", .second_name = "James"};
     printf("Person1's Age: %d\n", person1.age);
     printf("Person1's Salary: %0.2lf\n", person1.salary);
     printf("Person1's first_name: %s\n", person1.first_name);//it is possible to print the variable that contains the string using the puts function
-    printf("Person1's second_name: %s\n", person1.second_name);
-
-    puts(person1.first_name);
-    puts(person1.second_name);
+    printf("Person1's second_name: %s\n", person1.second_name);//for strings %s is used in order to print 
+    
+    //Demonstrating the function of puts
+    //puts(person1.first_name);
+    //puts(person1.second_name);
  
     return 0;
 }
